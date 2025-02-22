8d2d252b5fff4a8a5653a2e9873e056316a5c56b
Arthur Sardella <arthursardella@Arthurs-MacBook-Pro.local>
2025-02-21 20:08:45 -0700

    Finished working on Homework5

--- Homeworks/Homework5/CenterOfMass.py
+++ Homeworks/Homework5/CenterOfMass.py
@@ -1,11 +1,5 @@
-
-
 # Homework 4
-# Center of Mass Position and Velocity
-#  Solutions: G. Besla, R. Li, H. Foote
-
-
-
+#Arthur Sant'Ana
 # import modules
 import numpy as np
 import astropy.units as u
@@ -244,59 +238,6 @@ class CenterOfMass:
         # set the correct units using astropy
         # round all values
         return np.round(v_COM, 2)*u.km/u.s
-    
-
-
-# ANSWERING QUESTIONS
-#######################
-if __name__ == '__main__' : 
-
-
-    # Create  a Center of mass object for the MW, M31 and M33                                                              
-    MW_COM = CenterOfMass("MW_000.txt", 2)
-    M31_COM = CenterOfMass("M31_000.txt", 2)
-    M33_COM = CenterOfMass("M33_000.txt", 2)
-
-    # find and print the COM positions in phase-space for each galaxy
-
-    # MW 
-    MW_COM_p = MW_COM.COM_P(0.1)
-    MW_COM_v = MW_COM.COM_V(MW_COM_p[0],MW_COM_p[1],MW_COM_p[2])
-    print('MW COM xyz position:', MW_COM_p, 'and xyz velocity:', MW_COM_v)
-
-    # M31 
-    M31_COM_p = M31_COM.COM_P(0.1)
-    M31_COM_v = M31_COM.COM_V(M31_COM_p[0],M31_COM_p[1],M31_COM_p[2])
-    print('M31 COM xyz position:', M31_COM_p, 'and xyz velocity:', M31_COM_v)
-
-    # M33 
-    M33_COM_p = M33_COM.COM_P(0.1)
-    M33_COM_v = M33_COM.COM_V(M33_COM_p[0],M33_COM_p[1],M33_COM_p[2])
-    print('M33 COM xyz position:', M33_COM_p, 'and xyz velocity:', M33_COM_v)
-
-
-    # Q2 
-    # Determine the separation between the MW and M31                                                                      
-    MW_M31 = np.sqrt((M31_COM_p[0]-MW_COM_p[0])**2 + (M31_COM_p[1]-MW_COM_p[1])**2 + (M31_COM_p[2]-MW_COM_p[2])**2)
-    print("Separation between the MW and M31 =", np.round(MW_M31))
-
-    # Determine the relative velocity between the MW and M31                                                                      
-    vMW_M31 = np.sqrt((M31_COM_v[0]-MW_COM_v[0])**2 + (M31_COM_v[1]-MW_COM_v[1])**2 + (M31_COM_v[2]-MW_COM_v[2])**2)
-    print("Relative Velocity between the MW and M31 =", np.round(vMW_M31))
-
-
-    # Q3
-    # Determine the relative position between M33 and M31                                                                  
-    M33_M31 = np.sqrt((M33_COM_p[0]-M31_COM_p[0])**2 + (M33_COM_p[1]-M31_COM_p[1])**2 + (M33_COM_p[2]-M31_COM_p[2])**2)
-    print("Relative Position between M33 and M31 = ", np.round(M33_M31))
-
-
-    # Determine the relative velocity between M33 and M31                                                                  
-    vM33_M31 = np.sqrt((M33_COM_v[0]-M31_COM_v[0])**2 + (M33_COM_v[1]-M31_COM_v[1])**2 + (M33_COM_v[2]-M31_COM_v[2])**2)
-    print("Relative Velocity between M33 and M31 = ", np.round(vM33_M31))
-
-
-    # Q4:  The iterative procedue is necessary because as galaxies interact their stars get thrown to large radii with different speeds. Tidal tails are not spherically symmetric; this can cause the COM position calculation to be really off if you are using all the particles at large radii. 
 
 
 
---
+++ Homeworks/Homework5/ComponentMassFuncs.py
@@ -0,0 +1,78 @@
+import numpy as np
+import astropy.units as u
+import pandas as pd
+from ReadFile import Read
+
+
+def ComponentMass(filename, p_type):
+    '''
+    This function calculates the total mass of a certain particle type in a galaxy .txt file
+    Inputs:
+        filename (.txt file): name of the file referring to a galaxy simulation
+        p_type (int raging from 1 to 3): integer that refers to a particle type
+    Outputs:
+        TotMass (float): float containing the total mass for a given particle 
+            type in units of 10^12 solar masses
+    '''
+    
+    #Using the Read function from the previous assignment and storing data onto the 'data' variable
+    data = Read(filename)[2]
+    
+    #Indexing the data according to the particle type
+    index = np.where(data['type'] == p_type)
+    
+    #Creating a variable for the total mass to be stored
+    TotMass = 0
+    
+    #For loop iterating over every row and adding up the values
+    for i in data['m'][index]:
+        TotMass += i*10**10*u.Msun
+    
+    return np.around(TotMass/(10**12*u.Msun), 3)
+
+def MassFrame(filename_list):
+    '''
+    This function creates a pandas DataFrame for the galaxies in a list and with the
+        the rows being the type of particles
+    Input:
+        filename_list (python list of strings): this variable takes in a list containing strings
+            referring to .txt files containing data for galaxies
+            
+    Output:
+        df (pandas Dataframe): This function outputs a DataFrame with the Galaxy filenames being 
+            the columns and the particle types being the rows
+    '''
+    #Create a pandas DataFrame called df, having the appropriate columns and index being filename_list
+    column_list = ['Galaxy Name' , 'Halo Mass', 'Disk Mass', 'Bulge Mass', 'Sum', 'Fraction']
+    df = pd.DataFrame(columns = column_list, index = filename_list)
+    
+    #For loops appending values that come out of the ComponentMass function
+    #into the correct position of the DataFrame
+    
+    #For loop appending the first three values referring to each different particle type
+    for i in filename_list:
+        df.loc[i] = {'Halo Mass': ComponentMass(i, 1), 
+                     'Disk Mass':ComponentMass(i, 2), 
+                     'Bulge Mass':ComponentMass(i, 3)}
+    df.loc['M33_000.txt', 'Bulge Mass'] = 0
+    
+    #For loop appending values for the 'Sum' column
+    for i in filename_list:
+        df.loc[i, 'Sum'] = df.loc[i].sum()
+    
+    #For loop appending values for the 'Fraction' column
+    for k in filename_list:
+        df.loc[k, 'Fraction'] = np.around((df.loc[k, 'Bulge Mass']+df.loc[k, 'Disk Mass'])/(df.loc[k, 'Sum']), 3)
+    df['Galaxy Name'] = ['Milky Way','M31','M33']  
+    
+    #Reseting the index
+    df.index = [0,1,2]
+
+    #Renaming the columns so that they have the correct
+    #units
+    df.columns = (['Galaxy Name' , r'Halo Mass ($10^{12} M_{\odot}$)', 
+                                 r'Disk Mass ($10^{12} M_{\odot}$)', 
+                                r'Bulge Mass ($10^{12} M_{\odot}$)', 
+                                 r'Sum ($10^{12} M_{\odot}$)', 
+                                 r'Fraction ($10^{12} M_{\odot}$)'])
+    return df
